a10179af6de6c754e472193a667b7035f41aaffb
kshitij kumar <kshitijk83@gmail.com>
2019-03-17 00:48:28 +0530

    learning expressjs

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/add-product', (req, res, next)=>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'addProduct.html'));
+});
+
+router.post('/add-product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+});
+
+module.exports = router;
